158dbee45e53d733fb06138d6b6093c66b0391e5
Bruno Rocha <rochacbruno@gmail.com>
2025-08-05 16:09:37 +0100

    chore: style

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def photos():
     image_folder = os.path.join(app.static_folder, "imgs")
-    images = [os.path.join("imgs", img) for img in os.listdir(image_folder)]
+    images = [
+        os.path.join("imgs", img) 
+        for img in os.listdir(image_folder)
+    ]
     return render_template("photos.html", images=images)
 
 
